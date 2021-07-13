357317a116bea7716123449b7958a3f4c9578ee9
Newdigate <nicnewdigate@gmail.com>
2021-07-13 11:25:18 +0100

    add stereo

--- src/sampler.h
+++ src/sampler.h
@@ -21,31 +21,51 @@ private:
 
 class audiovoice {
 public:
+    audiovoice(AudioPlayArrayResmp *audioplayarray, AudioEffectEnvelope *audioenvelop,  AudioEffectEnvelope *audioenvelop2, AudioMixer4 *audiomixer, AudioMixer4 *audiomixer2, uint8_t mixerChannel, uint8_t mixerChannel2) :
+        _audioplayarray(audioplayarray), 
+        _audioenvelop(audioenvelop),
+        _audioenvelop2(audioenvelop2),
+        _audiomixer(audiomixer),
+        _audiomixer2(audiomixer2),
+        _mixerChannel(mixerChannel),
+        _mixerChannel2(mixerChannel2),
+        _isStereo(true) {
+    }
+
     audiovoice(AudioPlayArrayResmp *audioplayarray, AudioEffectEnvelope *audioenvelop, AudioMixer4 *audiomixer, uint8_t mixerChannel) :
         _audioplayarray(audioplayarray), 
         _audioenvelop(audioenvelop),
         _audiomixer(audiomixer),
-        _mixerChannel(mixerChannel) {
+        _mixerChannel(mixerChannel)
+    {
     }
 
     audiovoice(AudioPlayArrayResmp *audioplayarray) : 
-        _audioplayarray(audioplayarray), 
-        _audioenvelop(nullptr),
-        _audiomixer(nullptr),
-        _mixerChannel(0) {
+        _audioplayarray(audioplayarray) {
     }
 
     audiovoice(AudioPlayArrayResmp *audioplayarray, AudioMixer4 *audiomixer, uint8_t mixerChannel) :
         _audioplayarray(audioplayarray), 
-        _audioenvelop(nullptr),
         _audiomixer(audiomixer),
         _mixerChannel(mixerChannel) {
     }
+    audiovoice(AudioPlayArrayResmp *audioplayarray, AudioMixer4 *audiomixer, uint8_t mixerChannel, AudioMixer4 *audiomixer2, uint8_t mixerChannel2) :
+        _audioplayarray(audioplayarray), 
+        _audiomixer(audiomixer),
+        _audiomixer2(audiomixer2),
+        _mixerChannel(mixerChannel),
+        _mixerChannel2(mixerChannel2) {
+    }
+    AudioPlayArrayResmp *_audioplayarray = nullptr;
 
-    AudioPlayArrayResmp *_audioplayarray;
-    AudioEffectEnvelope *_audioenvelop;
-    AudioMixer4 *_audiomixer;
+    AudioEffectEnvelope *_audioenvelop = nullptr;
+    AudioEffectEnvelope *_audioenvelop2 = nullptr;
+    AudioMixer4 *_audiomixer = nullptr;
+    AudioMixer4 *_audiomixer2 = nullptr;
+
     uint8_t _mixerChannel;
+    uint8_t _mixerChannel2;
+    bool _isStereo = false;
 };
 
 class sampler {
@@ -66,6 +86,11 @@ public:
     void addSample(uint8_t noteNumber, int16_t *data, uint32_t sampleLength) {
         audiosample *newSample = new audiosample(noteNumber, data, sampleLength);
         _audiosamples.push_back(newSample);
+    }
+    void addVoice(AudioPlayArrayResmp &audioplayarrayresmp, AudioMixer4 &mixer, uint8_t mixerChannel, AudioEffectEnvelope &envelope, AudioMixer4 &mixer2, uint8_t mixerChannel2, AudioEffectEnvelope &envelope2) {
+
+        audiovoice *voice = new audiovoice(&audioplayarrayresmp, &envelope, &envelope2, &mixer, &mixer2, mixerChannel, mixerChannel2);
+        addVoice(voice) ;
     }
 
     void addVoice(AudioPlayArrayResmp &audioplayarrayresmp, AudioMixer4 &mixer, uint8_t mixerChannel, AudioEffectEnvelope &envelope) {
@@ -104,36 +129,49 @@ private:
     
     std::vector<audiosample*> _audiosamples;
 
-    static double calcFrequency(uint8_t note) {
-        double result = 440.0 * pow(2.0, (note-69) / 12.0);
+    static float calcFrequency(uint8_t note) {
+        float result = 440.0 * powf(2.0, (note-69) / 12.0);
         return result;
     }
 
-    static double calcPitchFactor(uint8_t note, uint8_t rootNoteNumber) {
-        double result = pow(2.0, (note-rootNoteNumber) / 12.0);
+    static float calcPitchFactor(uint8_t note, uint8_t rootNoteNumber) {
+        float result = powf(2.0, (note-rootNoteNumber) / 12.0);
         return result;
     }
+
     void noteEventCallback(uint8_t voice, uint8_t noteNumber, uint8_t velocity, bool isNoteOn, bool retrigger)
     {
         if (voice < _numVoices) {
             if (isNoteOn) {
                 audiosample *nearestSample = findNearestSampleForKey(noteNumber);
                 if (nearestSample != nullptr) {
-                    double factor = calcPitchFactor(noteNumber, nearestSample->_noteNumber);
+                    float factor = calcPitchFactor(noteNumber, nearestSample->_noteNumber);
                     _voices[voice]->_audioplayarray->setPlaybackRate(factor);
-                    _voices[voice]->_audioplayarray->play(nearestSample->_data, nearestSample->_sampleLength);
+                    
                     if (_voices[voice]->_audiomixer != nullptr) {                        
                         _voices[voice]->_audiomixer->gain(_voices[voice]->_mixerChannel, velocity / 255.0);
+                    }
+                    if (_voices[voice]->_audiomixer2 != nullptr) {                        
+                        _voices[voice]->_audiomixer2->gain(_voices[voice]->_mixerChannel, velocity / 255.0);
                     }
                     if (_voices[voice]->_audioenvelop != nullptr) {
                         _voices[voice]->_audioenvelop->noteOn();
                     }
+                    if (_voices[voice]->_audioenvelop2 != nullptr) {
+                        _voices[voice]->_audioenvelop2->noteOn();
+                    }
+                    _voices[voice]->_audioplayarray->play(nearestSample->_data, nearestSample->_sampleLength);
                 }
             } else {
                 // Note off event
                 if (_voices[voice]->_audioenvelop != nullptr) {
                     _voices[voice]->_audioenvelop->noteOff();
                 }
+                
+                if (_voices[voice]->_audioenvelop2 != nullptr) {
+                    _voices[voice]->_audioenvelop2->noteOff();
+                }
+
             }
         }
     }
